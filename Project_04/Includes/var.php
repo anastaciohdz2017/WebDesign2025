ab36923088c3c6ad1786cddce9975a45179344a0
Anastacio <Anastacio>
2025-11-13 09:41:58 -0600

    Project_04

---
+++ Project_04/Includes/var.php
@@ -0,0 +1,9 @@
+<?php
+$project= "Project 04 ";
+$nav = '<nav>
+        <ul>
+        <li><a href="index.php">Home Page</a></li>
+        <li><a href="aboutus.php">About Us</a></li>
+        <li><a href="contactus.php">Contact Us</a></li>
+        </ul>
+    </nav>';
---
+++ Project_04/contactus.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+    <head>
+        <?php
+        $page = "Contact Us ";
+        include_once'./includes/var.php';
+        ?>
+        <title>
+            <?php
+            echo $page."- ".$project;
+            ?>
+        </title>
+    </head>
+<body>
+    <nav>
+        <section id="header">
+    <?php
+    echo $nav;
+
+    ?>
+    </section>
+
+    </nav>
+
+<h1 style="text-align:center;"><?= "Welcome to ".$project.$page;?></h1>
+<section id="footer">
+    <?php
+    echo $nav;
+
+    ?>
+    </section>
+</body>
+</html>
